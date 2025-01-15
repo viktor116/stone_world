f4331f62b4e1578bb15b0782c199a8dc5e5784cc
soybean <984149293@qq.com>
2025-01-15 10:45:15 +0800

    1/14

--- src/main/java/com/stworld/Stone_world.java
+++ src/main/java/com/stworld/Stone_world.java
@@ -1,5 +1,6 @@
 package com.stworld;
 
+import com.stworld.event.EventRegister;
 import com.stworld.world.WorldRegister;
 import com.stworld.world.gen.ModWorldGeneration;
 import net.fabricmc.api.ModInitializer;
@@ -7,6 +8,7 @@ import net.fabricmc.api.ModInitializer;
 public class Stone_world implements ModInitializer {
 	@Override
 	public void onInitialize() {
+		EventRegister.initialize();
 		ModWorldGeneration.generateModWorldGen();
 	}
 }
---
+++ src/main/java/com/stworld/event/EventRegister.java
@@ -0,0 +1,21 @@
+package com.stworld.event;
+
+import com.stworld.world.WorldRegister;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+/**
+ * @author soybean
+ * @date 2025/1/15 10:35
+ * @description
+ */
+public class EventRegister {
+    public static void initialize() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+            player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(2.0);  // 设置最大生命值为2血（1颗心）
+            player.setHealth(2.0f); // 设置当前生命值为2血（1颗心）
+        });
+    }
+}
